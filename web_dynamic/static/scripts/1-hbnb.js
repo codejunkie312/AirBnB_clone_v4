82ed9d582d64fe90472d36f1f82d726d9a7b56d5
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-02-14 01:42:59 +0100

    added task 1

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  const amenities = {};
+  $('.container').on('change', 'input[type="checkbox"]', function() {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenities).join(', '));
+  });
+});
